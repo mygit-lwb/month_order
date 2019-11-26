b8cb88daf9fef4d59a64349f8e9839afe3f99ed4
lwb <2062626143@qq.com>
2019-11-26 14:09:31 +0800

    初始化

---
+++ src/main/java/com/lianweibo/month_order/MonthOrderApplication.java
@@ -0,0 +1,13 @@
+package com.lianweibo.month_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonthOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonthOrderApplication.class, args);
+    }
+
+}
